ac567d8a04790533d02fb6a73f1e0319b331e2cf
abdisetiakawan <bedikadiryt@gmail.com>
2024-09-19 10:25:41 +0700

    "Refactor routes to use controllers and middleware"

--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,13 @@
 const express = require("express");
-const { authenticateToken, authorizeRole } = require("../middleware/auth");
-const User = require("../models/User");
 const router = express.Router();
-// Menggunakan authenticateToken agar dapat melakukan verifikasi
+const { usersController } = require("../controllers");
+const { authenticateToken, authorizeRole } = require("../middleware/auth");
+
 router.use(authenticateToken);
 
-router.get("/", authorizeRole("penjual"), async (req, res) => {
-  try {
-    const users = await User.findAll();
-    res.status(200).json(users);
-  } catch (error) {
-    res.status(500).json({ message: "Error fetching users", error });
-  }
-});
-
-router.get("/:id", authorizeRole("penjual"), async (req, res) => {
-  try {
-    const user = await User.findByPk(req.params.id);
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(500).json({ message: "Error fetching user", error });
-  }
-});
+router.get(usersController.getUsers);
+router.get("/:id", usersController.getUserById);
+router.put("/:id", usersController.updateUser);
+router.delete("/:id", usersController.deleteUser);
 
 module.exports = router;
